62d916f6129e33983541729d070e3356b727fe91
Tmilgrew <thomasmilgrew@Thomas-Milgrews-MacBook-Pro-2.local>
2017-03-14 22:08:05 -0400

    Added the onClickListeners for the ImageButtons on the Battle Screen

--- app/src/main/java/example/androidstudio/swordmagicheart/MainActivity.java
+++ app/src/main/java/example/androidstudio/swordmagicheart/MainActivity.java
@@ -2,6 +2,8 @@ package example.androidstudio.swordmagicheart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
 
         //set screen to open on the battle screen
         setContentView(R.layout.battle_screen);
+
+        //----------------------------------------------------------
+        //finds the button with the rock image and establishes an
+        //on click listener
+        //----------------------------------------------------------
+        ImageButton rockButton = (ImageButton)findViewById(R.id.rock);
+        rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //----------------------------------------------------------
+                //add code to make main battle image the rock image
+                //----------------------------------------------------------
+            }
+        });
+
+        //----------------------------------------------------------
+        //finds the button with the paper image and establishes
+        // an on click listener
+        //----------------------------------------------------------
+        ImageButton paperButton = (ImageButton)findViewById(R.id.paper);
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //----------------------------------------------------------
+                //add code to make main battle image the paper image
+                //----------------------------------------------------------
+            }
+        });
+
+        //----------------------------------------------------------
+        //finds the button with the scissor image and establishes an
+        //on click listener
+        //----------------------------------------------------------
+        ImageButton scissorButton = (ImageButton)findViewById(R.id.scissor);
+        scissorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //----------------------------------------------------------
+                //add code to make main battle image the scissor image
+                //----------------------------------------------------------
+            }
+        });
     }
 }
