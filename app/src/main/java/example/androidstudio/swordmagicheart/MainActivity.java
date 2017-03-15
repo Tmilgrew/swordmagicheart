0ca1de44dbee0f558ca2c60c148173a26cf6ee60
Tmilgrew <thomasmilgrew@Thomas-Milgrews-MacBook-Pro-2.local>
2017-03-14 22:57:21 -0400

    added the feature to click on an option and show that option in the battle area

--- app/src/main/java/example/androidstudio/swordmagicheart/MainActivity.java
+++ app/src/main/java/example/androidstudio/swordmagicheart/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import static example.androidstudio.swordmagicheart.R.drawable.rock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,17 @@ public class MainActivity extends AppCompatActivity {
         //finds the button with the rock image and establishes an
         //on click listener
         //----------------------------------------------------------
-        ImageButton rockButton = (ImageButton)findViewById(R.id.rock);
+        final ImageButton rockButton = (ImageButton)findViewById(R.id.rock);
         rockButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //----------------------------------------------------------
                 //add code to make main battle image the rock image
                 //----------------------------------------------------------
+                ImageView playerChoice = (ImageView)findViewById(R.id.player_battle_choice);
+                playerChoice.setImageResource(R.drawable.rock);
+                playerChoice.setScaleX(-1);
+
             }
         });
 
@@ -32,13 +39,16 @@ public class MainActivity extends AppCompatActivity {
         //finds the button with the paper image and establishes
         // an on click listener
         //----------------------------------------------------------
-        ImageButton paperButton = (ImageButton)findViewById(R.id.paper);
+        final ImageButton paperButton = (ImageButton)findViewById(R.id.paper);
         paperButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //----------------------------------------------------------
                 //add code to make main battle image the paper image
                 //----------------------------------------------------------
+                ImageView playerChoice = (ImageView)findViewById(R.id.player_battle_choice);
+                playerChoice.setImageResource(R.drawable.paper);
+                playerChoice.setScaleX(1);
             }
         });
 
@@ -46,13 +56,16 @@ public class MainActivity extends AppCompatActivity {
         //finds the button with the scissor image and establishes an
         //on click listener
         //----------------------------------------------------------
-        ImageButton scissorButton = (ImageButton)findViewById(R.id.scissor);
+        final ImageButton scissorButton = (ImageButton)findViewById(R.id.scissor);
         scissorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //----------------------------------------------------------
                 //add code to make main battle image the scissor image
                 //----------------------------------------------------------
+                ImageView playerChoice = (ImageView)findViewById(R.id.player_battle_choice);
+                playerChoice.setImageResource(R.drawable.scissor);
+                playerChoice.setScaleX(1);
             }
         });
     }
